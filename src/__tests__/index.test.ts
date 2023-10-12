2c7b781ed405d66a4fddd4c553c83bffbd75e863
Felipe <feliperiverassilva@gmail.com>
2023-10-12 07:28:08 -0300

    chore: adding pokemon test trying error handler

--- src/__tests__/index.test.ts
+++ src/__tests__/index.test.ts
@@ -1,14 +1,43 @@
-import { getPokemonData } from '../index'
+import { getIsBabyEvolutionByPokemonName, getPokemonData } from '../index'
 
 describe('getPokemonData', () => {
   test('with valid data', async () => {
     // arrange
-    const pokemonName = 'ditsto '
+    const pokemonName = 'ditto'
 
     // act
     const result = await getPokemonData(pokemonName)
 
     // assert
     expect(result.id).toBe(132)
+  })
+  test('with invalid data', async () => {
+    // arrange
+    const pokemonName = 'dittos'
+
+    // act
+
+    // assert
+    await expect(getPokemonData(pokemonName)).rejects.toThrow('soy el error')
+  })
+  test('with invalid data', async () => {
+    // arrange
+    const pokemonName = 'dittos'
+
+    // act
+    const result = await getIsBabyEvolutionByPokemonName(pokemonName)
+
+    // assert
+    expect(result).toBe('I had a error')
+  })
+  test('with valid data', async () => {
+    // arrange
+    const pokemonName = 'ditto'
+
+    // act
+    const result = await getIsBabyEvolutionByPokemonName(pokemonName)
+
+    // assert
+    expect(result).toBe(false)
   })
 })
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,29 @@
 export async function getPokemonData(name: string): Promise<any> {
-  return (await fetch(`https://pokeapi.co/api/v2/pokemon/${name}`)).json()
+  try {
+    const result = await fetch(`https://pokeapi.co/api/v2/pokemon/${name}`)
+    return await result.json()
+  } catch(error) {
+    throw new Error('soy el error al obtener pokemon')
+  }
 }
+
+export async function getEvolution(id: number): Promise<any> {
+  try {
+    const result = await fetch(`https://pokeapi.co/api/v2/evolution-chain/${id}`)
+    return await result.json()
+  } catch(error) {
+    throw new Error('error al obtener la evolución')
+  }
+}
+
+export async function getIsBabyEvolutionByPokemonName(name: string): Promise<any> {
+  try {
+    const { id } = await getPokemonData(name)
+    const { chain: { is_baby }} = await getEvolution(id + 100000000)
+    return is_baby
+  } catch(error) {
+    console.log(error)
+    return 'I had a error'
+  }
+}
+
